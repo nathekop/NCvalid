05160a88ccde45c705614c0d7964347da48064c8
Nathakhun Wiroonsri <88187686+nwiroonsri@users.noreply.github.com>
2022-06-07 11:11:28 +0700

    Update NCI2.R

--- R/NCI2.R
+++ R/NCI2.R
@@ -1,4 +1,4 @@
-NCI2 <- function(x,kmax,kmin=2,method='kmeans',corr='pearson',nstart=100) {
+NCI2 <- function(x,kmax,kmin=2,method='kmeans',corr='pearson',nstart=100,NCstart=TRUE) {
   nw = NCvalid(x,kmax,kmin,method,corr,nstart)
   nw = nw$NCI2
 }
