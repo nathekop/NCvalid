b45b9890495ebfb2be9a7588d728f5c21413f308
Nathakhun Wiroonsri <88187686+nwiroonsri@users.noreply.github.com>
2022-06-08 14:47:53 +0700

    Update NCvalid.R

--- R/NCvalid.R
+++ R/NCvalid.R
@@ -47,7 +47,7 @@ NCvalid <- function(x,kmax,kmin=2,method='kmeans',corr='pearson',nstart=100 ,NCs
   K = length(crr)
   NWI = ((crr[2:(K-1)]-crr[1:(K-2)])/(1-crr[1:(K-2)]))/max(0,(crr[3:K]-crr[2:(K-1)])/(1-crr[2:(K-1)]))
   NWI2 = (crr[2:(K-1)]-crr[1:(K-2)])/(1-crr[1:(K-2)])-(crr[3:K]-crr[2:(K-1)])/(1-crr[2:(K-1)])
-  NWI3 = NWI1
+  NWI3 = NWI
   NWI3[NWI==Inf] = max(NWI[is.finite(NWI)])+NWI2[NWI==Inf]
   NWI3[NWI==-Inf] = min(NWI[is.finite(NWI)])
   NWI = data.frame(cbind("k"= kmin:kmax, "NCI1" = NWI))
