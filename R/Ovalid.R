b392b41d86901c7b15144bf3377b89e4e2802c9b
Nathakhun Wiroonsri <88187686+nwiroonsri@users.noreply.github.com>
2022-06-23 20:08:02 +0700

    Update Ovalid.R

--- R/Ovalid.R
+++ R/Ovalid.R
@@ -123,10 +123,10 @@ if (sum(indexlist == 'all')==1 | 'NC' %in% indexlist | 'NCI1' %in% indexlist | '
   crr = nw$NC
   NCI1 = nw$NCI1
   NCI2 = nw$NCI2
-  NCI3 = nw$NCI
+  NCI = nw$NCI
 }
 
-  my_list <- list("NC"=crr, "NCI" = NCI3, "NCI1" = NCI1, "NCI2" = NCI2, "CH" = CH, "CSL"=CSL, "DB"=DB, "DBs"=DBs, "DI"=DI, "GD33" = GD33, "GD43" = GD43, "GD53" = GD53, "PB"=PB, "SF"=SF,  "SC"=SC)
+  my_list <- list("NC"=crr, "NCI" = NCI, "NCI1" = NCI1, "NCI2" = NCI2, "CH" = CH, "CSL"=CSL, "DB"=DB, "DBs"=DBs, "DI"=DI, "GD33" = GD33, "GD43" = GD43, "GD53" = GD53, "PB"=PB, "SF"=SF,  "SC"=SC)
   if (sum(indexlist == "all")==1){
     return(my_list)
   } else {
